ff014a9d136fa915606b91f2b78d8637cbb9b1c0
songjein <jeinsong100@gmail.com>
2016-11-25 15:51:05 +0000

    basic + mesh example

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('obj', { title: 'Express' });
 });
 
-router.get('/2', function(req, res, next) {
-  res.render('index2');
+router.get('/obj', function(req, res, next) {
+  res.render('obj', { title: 'Express' });
+});
+
+router.get('/mesh', function(req, res, next) {
+  res.render('mesh');
 });
 
 router.get('/3', function(req, res, next) {
